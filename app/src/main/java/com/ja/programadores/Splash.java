bc1071816559c98223bf24b17ba4cca92c438191
Jack <40079880+jacksrlt@users.noreply.github.com>
2022-05-26 04:03:39 +0200

    Created pop up for post

---
+++ app/src/main/java/com/ja/programadores/Splash.java
@@ -0,0 +1,35 @@
+package com.ja.programadores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        openApp(true);
+    }
+
+    private void openApp(boolean locationPermission) {
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash
+                        .this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
